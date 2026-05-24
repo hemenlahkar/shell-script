7514cbbad7da26b6b752fe47f9069546eae94325
Hemen Lahkar <hemenlahkar3108+github@gmail.com>
2026-05-24 22:56:58 +0530

    add 2nd practical file

---
+++ practical list/02_utility_commands.sh
@@ -0,0 +1,17 @@
+cal
+# shows calendar of the current month
+
+cat 'file1.txt' 'file2.txt'
+# concatenates file1.txt and file2.txt files and prints on the standard output
+
+mv 'file1.txt' './dir/'
+# moves file1 to ./dir
+
+cp 'file1.txt' 'file-c.txt'
+# copies content of file1.txt to 'file-c.txt'
+
+man
+# manual pages
+
+date
+# shows current date and time
