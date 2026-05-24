34455ff69489e44bce96d4825d9894d8d4bd845a
Hemen Lahkar <hemenlahkar3108+github@gmail.com>
2026-05-24 23:41:05 +0530

    add variable to greeting script

--- bestdayever.sh
+++ bestdayever.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-echo "Good Morning Hemen!!"
+name="Patricia"
+
+echo "Good Morning $name!!"
 
 sleep 1
 
-echo "You're looking good today Hemen!!"
+echo "You're looking good today $name!!"
 
 sleep 1
 
-echo "You have the best beard I've ever seen Hemen!!"
+echo "You have the best beard I've ever seen $name!!"
