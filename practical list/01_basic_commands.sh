33c16bcc347f7b184e908f000becf4c3bb8363cf
Hemen Lahkar <hemenlahkar3108+github@gmail.com>
2026-05-24 22:24:23 +0530

    add first practical file

---
+++ practical list/01_basic_commands.sh
@@ -0,0 +1,30 @@
+ls
+# outputs list of files and directories in the current working directory
+
+pwd
+# prints the Present Working Directory
+
+tty
+# prints the file name of the terminal connected to standard output
+
+who
+# prints the currently logged in user
+
+whoami
+# prints the username of current user
+
+rm 'file.txt'
+# removes file.txt
+
+mkdir 'directory name'
+# creates a new directory named 'directory name'
+
+rmdir 'directory name'
+# removes 'directory name' if empty
+
+touch 'file.txt'
+# create file, or change file timestamp
+
+cd 'dir'
+# change the working directory to ./dir
+
