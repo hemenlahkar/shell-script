e1b8c0a1277140512cad321192571e0901e2abac
Hemen Lahkar <hemenlahkar3108+github@gmail.com>
2026-05-24 23:07:39 +0530

    add 3rd practical file

---
+++ practical list/03_utilities.sh
@@ -0,0 +1,12 @@
+chmod 'file.txt' 777
+# changes file permission for current user, current group and all users
+
+grep "pattern" 'file.txt'
+# prints lines of file.txt that matches pattern
+
+tput
+# initialize a terminal, or query terminfo database
+
+bc
+# arbitrary-precision arithmatic language
+
