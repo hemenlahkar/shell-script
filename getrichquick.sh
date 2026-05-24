f7205e9a225fce5153b2965bd42a80a3f9ff0919
Hemen Lahkar <hemenlahkar3108+github@gmail.com>
2026-05-25 02:48:21 +0530

    add millionare code

---
+++ getrichquick.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo "What is your name?"
+read name
+
+echo "What is your age?"
+read age
+
+echo "Hello $name, you are $age years old."
+
+sleep 2
+
+echo "Calculating"
+echo ".........."
+sleep 1
+echo "**........"
+sleep 1
+echo "****......"
+sleep 1
+echo "******...."
+sleep 1
+echo "********.."
+sleep 1
+echo "**********"
+
+getrich=$(( $RANDOM % 15 + $age))
+
+echo "$name, you will become a millionare when you are $getrich years old"
