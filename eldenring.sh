039f70801e8eeee603b4650b574168c670cd34e7
Hemen Lahkar <hemenlahkar3108+github@gmail.com>
2026-05-25 23:22:14 +0530

    add nested conditions

--- eldenring.sh
+++ eldenring.sh
@@ -2,12 +2,36 @@
 
 echo "You Died!"
 
-echo "Hey, do you like coffee? (y/n)"
+# First beast battle
 
-read coffee
+beast=$(( $RANDOM % 2 ))
 
-if [[ $coffee == "y" ]]; then
-	echo "You're awesome"
+echo "You first approaches. Prepare to battle. Pick a number between 0-1. (0/1)"
+read tarnished
+
+if [[ $beast == $tarnished && 47 > 28 ]]; then
+	echo "Beast VANQUISHED!! Congrats fellow tarnished"
 else
-	echo "Leave right now!!!"
+	echo "Beast chose $beast - You Died"
+	exit 1
 fi
+
+# Second beast battle
+sleep 2
+
+echo "Boss battle. Get scared. It's Margit. Pick a number between 0-9. (0-9)"
+
+read tarnished
+
+beast=$(( $RANDOM % 10 ))
+
+if [[ $beast == $tarnished || $tarnished == "coffee" ]]; then
+	if [[ $USER == "root" ]]; then
+		echo "Beast VANQUISHED!!!"
+	fi
+elif [[ $USER == "hemen" ]]; then
+	echo "Hey, Hemen always wins. You vanquished beast."
+else
+	echo "You Died!!"
+fi
+
