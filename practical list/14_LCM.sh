db68fde154a64dd6812667671a979d0ca4dcd000
Hemen Lahkar <hemenlahkar3108+github@gmail.com>
2026-05-26 06:58:11 +0530

    add HCF along with LCM

--- practical list/14_LCM.sh
+++ practical list/14_LCM.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Find the LCM of two numbers
+# Find the LCM & HCF of two numbers
 
 printf "Enter the first number: "
 read a
@@ -12,13 +12,15 @@ if [ $a -gt $b ]; then
 	b=$temp
 fi
 
-x=$b
-while [ $x -le $(( a * b )) ]; do
-	if [ $(( x % a )) -eq 0 ]; then
-		lcm=$x
-		break
-	fi
-	x=$(( x + b ))
+x=$a
+y=$b
+while [ $y -ne 0 ]; do
+	temp=$y
+	y=$(( x % y))
+	x=$temp
 done
+hcf=$x
+lcm=$(( a * b / hcf ))
 
 echo "LCM($a, $b) = $lcm"
+echo "HCF($a, $b) = $hcf"
